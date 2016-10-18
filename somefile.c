af7895d7662a7ea10a311a7036deeb9b6b8eff6e
Iain Scott <iain@lucidpartnership.co.uk>
2016-10-18 10:39:11 +0100

    Added function A

--- somefile.c
+++ somefile.c
@@ -10,9 +10,15 @@ void func2(void)
 	cprintf("This is function %d\n",2);
 	}
 
+void funcA(void)
+	{
+	cprintf("This is function %c\n",41);
+	}
+
 int main(void)
 	{
 	cprintf("%s","this is a string\r\n");
 	func1();
 	func2();
+	funcA();
 	}
